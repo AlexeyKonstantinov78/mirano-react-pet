af59bddbcf1c23ba333aaedaa8f81ac236649f4a
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-07-03 20:02:27 +0300

    update style add style cartitem

--- src/modules/CartItem/CartItem.jsx
+++ src/modules/CartItem/CartItem.jsx
@@ -1,13 +1,15 @@
-export const CartItem = ({ _, img, title, price }) => (
+import _ from './CartItem.module.scss';
+
+export const CartItem = ({ img, title, price }) => (
   <li className={_.cart__item}>
     <img className={_.cart__img}
       src={img}
       alt={title} />
-    <h4 className={_.cart__itemTitle}>{title}</h4>
+    <h4 className={_["cart__item-title"]}>{title}</h4>
     <div className={_.cart__counter}><button
-      className={_.cart__counterBtn}>-</button><input
-        className={_.cart__counterInput} type="number" max="99" min="0"
-        value="1" /><button className={_.cart__counterBtn}>+</button></div>
+      className={_["cart__counter-btn"]}>-</button><input
+        className={_["cart__counter-input"]} type="number" max="99" min="0"
+        value="1" /><button className={_["cart__counter-btn"]}>+</button></div>
     <p className={_.cart__price}>{price}&nbsp;₽</p>
   </li>
 );
