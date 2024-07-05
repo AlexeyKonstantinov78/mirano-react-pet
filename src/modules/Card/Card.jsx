92014b0066b3dd6774b2e71ffe337d54b1f51fee
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-07-05 21:14:06 +0300

    update card

--- src/modules/Card/Card.jsx
+++ src/modules/Card/Card.jsx
@@ -1,18 +1,22 @@
 import _ from './Card.module.scss';
 
-export const Card = ({ className = '', img, title, dateDelivery, price }) => (
-  <article className={`${className} ${_.card}`}>
-    <img className={_.card__image}
-      src={img}
-      alt={title} />
-    <div className={_.card__content}>
-      <h3 className={_.card__title}>{title}
-      </h3>
-      <div className={_.card__footer}>
-        <p className={_.card__dateDelivery}>{dateDelivery}</p><button
-          className={_.card__button}>{price}&nbsp;₽</button>
+export const Card = ({ className = '', id, img, title, dateDelivery, price }) => {
+
+
+  return (
+    <article className={`${className} ${_.card}`}>
+      <img className={_.card__image}
+        src={img}
+        alt={title} />
+      <div className={_.card__content}>
+        <h3 className={_.card__title}>{title}
+        </h3>
+        <div className={_.card__footer}>
+          <p className={_.card__dateDelivery}>{dateDelivery}</p><button
+            className={_.card__button}>{price}&nbsp;₽</button>
+        </div>
       </div>
-    </div>
-  </article>
-);
+    </article>
+  );
+}
 
