ed0091fcead174da35d416cbbcc269cc22366a6e
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-07-12 19:40:31 +0300

    registr cart cookie

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,10 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { KEY_LOCAL_STORAGE } from '../const';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API_URL_RENDER, KEY_LOCAL_STORAGE } from '../const';
 
 const initialState = {
   isOpen: false,
   items: JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE) || '[]'),
+  status: 'idle',
+  accessKey: null,
+  error: null,
 };
+
+export const registerCart = createAsyncThunk(
+  'cart/registerCart',
+  async () => {
+    const response = await fetch(`${API_URL_RENDER}/api/cart/register`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    return await response.json();
+  });
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -14,7 +31,6 @@ const cartSlice = createSlice({
       state.isOpen = !state.isOpen;
     },
     addItemToCart(state, action) {
-
       const {
         id,
         img,
@@ -40,6 +56,22 @@ const cartSlice = createSlice({
 
       localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(state.items));
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerCart.pending, (state) => {
+        state.status = 'loading',
+          state.error = null;
+      })
+      .addCase(registerCart.fulfilled, (state, action) => {
+        state.status = 'success';
+        state.accessKey = action.payload.accessKey;
+      })
+      .addCase(registerCart.rejected, (state, action) => {
+        state.status = 'failed';
+        state.accessKey = '';
+        state.error = action.error.message;
+      });
   },
 });
 
