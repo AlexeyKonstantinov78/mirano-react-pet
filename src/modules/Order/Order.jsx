382e0cc207fe3e4e9f4217144e51c3487cfda4de
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-07-18 18:49:41 +0300

    feat add send order

--- src/modules/Order/Order.jsx
+++ src/modules/Order/Order.jsx
@@ -1,17 +1,28 @@
 import { useDispatch, useSelector } from 'react-redux';
 import _ from './Order.module.scss';
-import { closeModal } from '../../store/orderSlice';
+import { closeModal, sendOrder, updateOrderData } from '../../store/orderSlice';
 import { useCallback, useEffect } from 'react';
-
 
 export const Order = () => {
   const dispatch = useDispatch();
-  const isOpen = useSelector(state => state.order.isOpen);
+  const { isOpen, orderId, data } = useSelector((state) => state.order);
+  const totalCartPrice = useSelector((state) => state.cart.totalCartPrice);
   const isOrderReady = false;
 
   const handlerCloseOrder = useCallback(() => {
     dispatch(closeModal());
   }, [dispatch]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    dispatch(updateOrderData({ [name]: value }));
+  };
+
+  const handleSubmitOrder = (e) => {
+    e.preventDefault();
+    dispatch(sendOrder());
+  }
 
   useEffect(() => {
     const handleEsc = (e) => {
@@ -29,84 +40,147 @@ export const Order = () => {
     };
   }, [isOpen, handlerCloseOrder]);
 
-
   if (!isOpen) {
     return null;
   }
 
   return (
     <div className={_.order} onClick={handlerCloseOrder}>
-      <div className={_.order__wrapper} onClick={(e) => { e.stopPropagation() }}>
-        {isOrderReady ?
+      <div
+        className={_.order__wrapper}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}>
+        {orderId ? (
           <>
             <h2 className={_.order__title}>Заказ оформлен!</h2>
-            <p className={_.order__id}>Ваш номер заказа:
-              971f365a-caa1-4cdb-9446-bad2eff047e1</p>
-          </> :
+            <p className={_.order__id}>Ваш номер заказа: {orderId}</p>
+          </>
+        ) : (
           <>
             <h2 className={_.order__title}>Оформить заказ</h2>
-            <form className={_.order__form} id="order">
+            <form className={_.order__form} id='order' onSubmit={handleSubmitOrder}>
               <fieldset className={_.order__fieldset}>
                 <legend className={_.order__legend}>Данные заказчика</legend>
                 <div className={_.order__inputGroup}>
-                  <input className={_.order__input}
-                    type="text" name="name-buyer" placeholder="Имя" />
-                  <input className={_.order__input} type="text" name="phone-buyer"
-                    placeholder="Телефон" />
+                  <input
+                    className={_.order__input}
+                    type='text'
+                    name='buyerName'
+                    placeholder='Имя'
+                    value={data.buyerName}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className={_.order__input}
+                    type='text'
+                    name='buyerPhone'
+                    placeholder='Телефон'
+                    value={data.buyerPhone}
+                    onChange={handleChange}
+                  />
                 </div>
               </fieldset>
               <fieldset className={_.order__fieldset}>
                 <legend className={_.order__legend}>Данные получателя</legend>
                 <div className={_.order__inputGroup}>
-                  <input className={_.order__input}
-                    type="text" name="name-recipient" placeholder="Имя" />
                   <input
-                    className={_.order__input} type="text" name="phone-recipient"
-                    placeholder="Телефон" />
+                    className={_.order__input}
+                    type='text'
+                    name='recipientName'
+                    placeholder='Имя'
+                    value={data.recipientName}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className={_.order__input}
+                    type='text'
+                    name='recipientPhone'
+                    placeholder='Телефон'
+                    value={data.recipientPhone}
+                    onChange={handleChange}
+                  />
                 </div>
               </fieldset>
               <fieldset className={_.order__fieldset}>
                 <legend className={_.order__legend}>Адрес</legend>
                 <div className={_.order__inputGroup}>
-                  <input className={_.order__input}
-                    type="text" name="street" placeholder="Улица" />
                   <input
-                    className={_.order__input + ' ' + _.order__input_min} type="text" name="house"
-                    placeholder="Дом" />
-                  <input className={_.order__input + ' ' + _.order__input_min}
-                    type="text" name="apartment" placeholder="Квартира" />
+                    className={_.order__input}
+                    type='text'
+                    name='street'
+                    placeholder='Улица'
+                    value={data.street}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className={_.order__input + ' ' + _.order__input_min}
+                    type='text'
+                    name='house'
+                    placeholder='Дом'
+                    value={data.house}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className={_.order__input + ' ' + _.order__input_min}
+                    type='text'
+                    name='apartment'
+                    placeholder='Квартира'
+                    value={data.apartment}
+                    onChange={handleChange}
+                  />
                 </div>
               </fieldset>
               <fieldset className={_.order__fieldset}>
                 <div className={_.order__payment}>
                   <label className={_.order__labelRadio}>
                     <input
-                      className={_.order__radio} type="radio" name="payment-online"
-                      value="true" defaultChecked />Оплата онлайн</label>
+                      className={_.order__radio}
+                      type='radio'
+                      name='paymentOnline'
+                      value={data.paymentOnline === 'true'}
+                      defaultChecked
+                      onChange={handleChange}
+                    />
+                    Оплата онлайн
+                  </label>
                 </div>
                 <div className={_.order__delivery}>
-                  <label htmlFor="delivery">Доставка 01.07</label>
-                  <input type="hidden" name="delivery-date" value="01.07" />
+                  <label htmlFor='delivery'>Доставка 01.07</label>
+                  <input
+                    type='hidden'
+                    name='deliveryDate'
+                    value={data.deliveryDate}
+                    onChange={handleChange}
+                  />
                   <div className={_.order__selectWrapper}>
-                    <select className={_.order__select}
-                      name="delivery-time" id="delivery">
-                      <option value="9-12">с 9:00 до 12:00</option>
-                      <option value="12-15">с 12:00 до 15:00</option>
-                      <option value="15-18">с 15:00 до 18:00</option>
-                      <option value="18-21">с 18:00 до 21:00</option>
+                    <select
+                      className={_.order__select}
+                      name='deliveryTime'
+                      id='delivery'
+                      value={data.deliveryTime}
+                      onChange={handleChange}>
+                      <option value='9-12'>с 9:00 до 12:00</option>
+                      <option value='12-15'>с 12:00 до 15:00</option>
+                      <option value='15-18'>с 15:00 до 18:00</option>
+                      <option value='18-21'>с 18:00 до 21:00</option>
                     </select>
                   </div>
                 </div>
               </fieldset>
             </form>
+            <div className={_.order__footer}>
+              <p className={_.order__total}>{totalCartPrice}&nbsp;₽</p>
+              <button className={_.order__button} type='submit' form='order'>
+                Заказать
+              </button>
+            </div>
           </>
-        }
-        <div className={_.order__footer}>
-          <p className={_.order__total}>92100&nbsp;₽</p><button className={_.order__button}
-            type="submit" form="order">Заказать</button>
-        </div>
+        )}
       </div>
-      <button className={_.order__close} type="button" >×</button>
+      <button className={_.order__close} type='button'>
+        ×
+      </button>
     </div>
   );
-}
+};
--- src/store/orderSlice.js
+++ src/store/orderSlice.js
@@ -1,8 +1,79 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API_URL_RENDER, TEXT_ERROR } from '../const';
+import { fetchCart, toggleCart } from './cartSlice';
 
 const initialState = {
   isOpen: false,
+  orderId: '',
+  data: {
+    buyerName: '',
+    buyerPhone: '',
+    recipientName: '',
+    recipientPhone: '',
+    street: '',
+    house: '',
+    apartment: '',
+    paymentOnline: 'true',
+    deliveryDate: '',
+    deliveryTime: '',
+  },
+  error: null,
 };
+
+export const sendOrder = createAsyncThunk(
+  'order/sendOrder',
+  async (_, { getState, dispatch }) => {
+    const {
+      order: {
+        data: {
+          buyerName,
+          buyerPhone,
+          recipientName,
+          recipientPhone,
+          street,
+          house,
+          apartment,
+          paymentOnline,
+          deliveryDate,
+          deliveryTime,
+        }
+      }
+    } = getState();
+    const orderData = {
+      "buyer": {
+        "name": buyerName,
+        "phone": buyerPhone,
+      },
+      "recipient": {
+        "name": recipientName,
+        "phone": recipientPhone,
+      },
+      "address": `${street}, ${house}, ${apartment}`,
+      paymentOnline,
+      deliveryDate,
+      deliveryTime,
+    };
+
+    const response = await fetch(`${API_URL_RENDER}/api/orders`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(orderData),
+    });
+
+    if (!response.ok) {
+      throw new Error(TEXT_ERROR);
+    }
+
+    dispatch(clearOrder());
+    dispatch(toggleCart());
+    dispatch(fetchCart());
+
+    return await response.json();
+  }
+);
 
 const orderSlice = createSlice({
   name: 'order',
@@ -17,9 +88,43 @@ const orderSlice = createSlice({
     toggleOrder(state) {
       state.isOpen = !state.isOpen;
     },
+    clearOrder(state) {
+      state.data = {
+        buyerName: '',
+        buyerPhone: '',
+        recipientName: '',
+        recipientPhone: '',
+        street: '',
+        house: '',
+        apartment: '',
+        paymentOnline: 'true',
+        deliveryDate: '',
+        deliveryTime: '',
+      };
+      state.orderId = '';
+    },
+    updateOrderData(state, action) {
+      // state.data[action.payload.name] = action.payload.value;
+      state.data = { ...state.data, ...action.payload };
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(sendOrder.pending, (state) => {
+        state.orderId = '';
+        state.error = null;
+      })
+      .addCase(sendOrder.fulfilled, (state, action) => {
+        console.log(action);
+        state.orderId = action.payload.orderId;
+      })
+      .addCase(sendOrder.rejected, (state, action) => {
+        state.error = action.error.message;
+        state.orderId = '';
+      });
   },
 });
 
-export const { toggleOrder, openModal, closeModal } = orderSlice.actions;
+export const { toggleOrder, openModal, closeModal, clearOrder, updateOrderData } = orderSlice.actions;
 
 export default orderSlice.reducer;
